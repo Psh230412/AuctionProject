997135bacd842ff6dce2dc31d95d47dd1547aec3
Jisu-Seong <sjangela0615@gmail.com>
2023-06-27 10:43:03 +0900

    마이페이지 리스트 불러오기 추가

---
+++ 0623Start/src/EnrollParticipate.java
@@ -0,0 +1,87 @@
+
+
+// 유저가 직접 등록한 물품 관련 정보 or
+// 유저가 참여한 입찰 - 다른 유저가 등록한 물품을 현재 유저가 입찰하므로 담는 정보가 Enrollment와 다름
+
+import java.time.LocalDateTime;
+
+import javax.swing.ImageIcon;
+
+public class EnrollParticipate {
+	private Integer setno;
+	private Integer userno;
+	private Integer productno;
+	private String productname;
+	private ImageIcon image;
+	private LocalDateTime endTime;
+	private int productPriceNow;
+
+	public EnrollParticipate(Integer setno, Integer userno, Integer productno, String productname, ImageIcon image,
+			LocalDateTime endTime, int productPriceNow) {
+		super();
+		this.setno = setno;
+		this.userno = userno;
+		this.productno = productno;
+		this.productname = productname;
+		this.image = image;
+		this.endTime = endTime;
+		this.productPriceNow = productPriceNow;
+	}
+
+	public Integer getSetno() {
+		return setno;
+	}
+
+	public void setSetno(Integer setno) {
+		this.setno = setno;
+	}
+
+	public Integer getUserno() {
+		return userno;
+	}
+
+	public void setUserno(Integer userno) {
+		this.userno = userno;
+	}
+
+	public Integer getProductno() {
+		return productno;
+	}
+
+	public void setProductno(Integer productno) {
+		this.productno = productno;
+	}
+
+	public String getProductname() {
+		return productname;
+	}
+
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+
+	public ImageIcon getImage() {
+		return image;
+	}
+
+	public void setImage(ImageIcon image) {
+		this.image = image;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getProductPriceNow() {
+		return productPriceNow;
+	}
+
+	public void setProductPriceNow(int productPriceNow) {
+		this.productPriceNow = productPriceNow;
+	}
+
+}
