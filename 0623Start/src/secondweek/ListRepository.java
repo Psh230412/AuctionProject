500a7a01141a14a67eff9225651c802e30f56482
JeongBin0462 <dlwjdqls783@gmail.com>
2023-06-29 10:09:02 +0900

    MyPage 낙찰/유찰 변경

--- 0623Start/src/secondweek/ListRepository.java
+++ 0623Start/src/secondweek/ListRepository.java
@@ -28,7 +28,9 @@ public class ListRepository{
 	public List<EnrollParticipate> getEnrollment(int userno) {
 		Connection conn = null;
 		PreparedStatement stmt = null;
+		PreparedStatement stmt2 = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
 		List<EnrollParticipate> list = new ArrayList<>();
 		try {
 			conn = DBUtil.getConnection();
@@ -37,7 +39,7 @@ public class ListRepository{
 					"INNER JOIN product C ON B.productno = C.productno \r\n" + 
 					"INNER JOIN copy_auction D ON B.setno = D.setno\r\n" + 
 					"WHERE A.userno = ?\r\n"
-					+ "AND D.deadline > CURRENT_TIME()\r\n"
+					+ "AND D.deadline > current_timestamp()\r\n"
 					+ "ORDER BY D.deadline";
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, userno);
@@ -60,10 +62,44 @@ public class ListRepository{
 				list.add(new EnrollParticipate(setnoParse, usernoParse, productnoParse, productname, imageIcon, endTime,
 						finalprice));
 			}
+			String sql2 = "SELECT B.setno, A.userno, C.productno, C.productname, C.image, D.deadline, D.finalprice FROM user A\n" + 
+					"INNER JOIN enrollmentinfo B ON A.userno = B.userno\n" + 
+					"INNER JOIN product C ON B.productno = C.productno\n" + 
+					"INNER JOIN copy_auction D ON B.setno = D.setno\n" + 
+					"WHERE A.userno = ?\n" + 
+					"AND TIMESTAMPDIFF(SECOND, D.deadline, current_timestamp()) > 0\n" + 
+					"AND DATEDIFF(D.deadline, current_timestamp()) >= -7\n" + 
+					"ORDER BY D.deadline DESC;";
+
+			stmt2 = conn.prepareStatement(sql2);
+			stmt2.setInt(1, userno);
+			rs2 = stmt2.executeQuery();
+
+			while (rs2.next()) { 
+			    Integer setnoParse = rs2.getObject("setno", Integer.class);
+			    Integer usernoParse = rs2.getObject("userno", Integer.class);
+			    Integer productnoParse = rs2.getObject("productno", Integer.class);
+			    String productname = rs2.getString("productname");
+			    Blob imageBlob = rs2.getBlob("image");
+
+			    byte[] imageBytes = imageBlob.getBytes(1, (int) imageBlob.length());
+			    ImageIcon imageIcon = setImage(imageBytes);
+
+			    int finalprice = rs2.getInt("finalprice");
+
+			    LocalDateTime endTime = rs2.getObject("deadline", LocalDateTime.class);
+
+			    list.add(new EnrollParticipate(setnoParse, usernoParse, productnoParse, productname, imageIcon, endTime,
+			            finalprice));
+			}
+			
+			
 			return list;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
+			DBUtil.close(rs2);
+			DBUtil.close(stmt2);
 			DBUtil.close(rs);
 			DBUtil.close(stmt);
 			DBUtil.close(conn);
@@ -75,19 +111,29 @@ public class ListRepository{
 	public List<EnrollParticipate> getParticipateList(int userno) {
 		Connection conn = null;
 		PreparedStatement stmt = null;
+		PreparedStatement stmt2 = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
 		List<EnrollParticipate> list = new ArrayList<>();
 		try {
 			conn = DBUtil.getConnection();
-			String sql = "SELECT C.setno, A.userno, E.productno, E.productname, E.image, C.finalprice, C.deadline \n" + 
-					"FROM user A\n" + 
-					"INNER JOIN participate B ON A.userno = B.userno\n" + 
-					"INNER JOIN copy_auction C ON B.auctionno = C.auctionno\n" + 
-					"INNER JOIN enrollmentinfo D ON C.setno = D.setno\n" + 
-					"INNER JOIN product E ON D.productno = E.productno\n" + 
-					"WHERE A.userno = ?\n" + 
-					"AND C.deadline > CURRENT_TIME()\n" + 
-					"ORDER BY C.deadline";
+			String sql = "SELECT C.setno, A.userno, E.productno, E.productname, E.image, C.finalprice, C.deadline\n"
+					+ "FROM user A\n"
+					+ "INNER JOIN (\n"
+					+ "    SELECT p.*\n"
+					+ "    FROM participate p\n"
+					+ "    INNER JOIN (\n"
+					+ "        SELECT auctionno, MAX(participatetime) AS max_time\n"
+					+ "        FROM participate\n"
+					+ "        GROUP BY auctionno\n"
+					+ "    ) AS PP ON p.auctionno = PP.auctionno AND p.participatetime = PP.max_time\n"
+					+ ") AS B ON A.userno = B.userno\n"
+					+ "INNER JOIN copy_auction C ON B.auctionno = C.auctionno\n"
+					+ "INNER JOIN enrollmentinfo D ON C.setno = D.setno\n"
+					+ "INNER JOIN product E ON D.productno = E.productno\n"
+					+ "WHERE A.userno = ?\n"
+					+ "AND C.deadline > CURRENT_TIME()\n"
+					+ "ORDER BY C.deadline;";
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, userno);
 			rs = stmt.executeQuery();
@@ -109,9 +155,54 @@ public class ListRepository{
 				list.add(new EnrollParticipate(setnoParse, usernoParse, productnoParse, productname, imageIcon, endTime,
 						finalprice));
 			}
+			String sql2 = "SELECT C.setno, A.userno, E.productno, E.productname, E.image, C.finalprice, C.deadline\n" + 
+					"FROM user A\n" + 
+					"INNER JOIN (\n" + 
+					"    SELECT p.*\n" + 
+					"    FROM participate p\n" + 
+					"    INNER JOIN (\n" + 
+					"        SELECT auctionno, MAX(participatetime) AS max_time\n" + 
+					"        FROM participate\n" + 
+					"        GROUP BY auctionno\n" + 
+					"    ) AS PP ON p.auctionno = PP.auctionno AND p.participatetime = PP.max_time\n" + 
+					") AS B ON A.userno = B.userno\n" + 
+					"INNER JOIN copy_auction C ON B.auctionno = C.auctionno\n" + 
+					"INNER JOIN enrollmentinfo D ON C.setno = D.setno\n" + 
+					"INNER JOIN product E ON D.productno = E.productno\n" + 
+					"WHERE A.userno = ?\n" + 
+					"AND TIMESTAMPDIFF(SECOND, C.deadline, current_timestamp()) > 0\n" + 
+					"AND DATEDIFF(C.deadline, current_timestamp()) >= -7\n" + 
+					"ORDER BY C.deadline DESC;";
+
+			stmt2 = conn.prepareStatement(sql2);
+			stmt2.setInt(1, userno);
+			rs2 = stmt2.executeQuery();
+
+			while (rs2.next()) { 
+			    Integer setnoParse = rs2.getObject("setno", Integer.class);
+			    Integer usernoParse = rs2.getObject("userno", Integer.class);
+			    Integer productnoParse = rs2.getObject("productno", Integer.class);
+			    String productname = rs2.getString("productname");
+			    Blob imageBlob = rs2.getBlob("image");
+
+			    byte[] imageBytes = imageBlob.getBytes(1, (int) imageBlob.length());
+			    ImageIcon imageIcon = setImage(imageBytes);
+
+			    int finalprice = rs2.getInt("finalprice");
+
+			    LocalDateTime endTime = rs2.getObject("deadline", LocalDateTime.class);
+
+			    list.add(new EnrollParticipate(setnoParse, usernoParse, productnoParse, productname, imageIcon, endTime,
+			            finalprice));
+			}
+			
+			
+			return list;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
+			DBUtil.close(rs2);
+			DBUtil.close(stmt2);
 			DBUtil.close(rs);
 			DBUtil.close(stmt);
 			DBUtil.close(conn);
@@ -120,47 +211,47 @@ public class ListRepository{
 	}
 
 	// 낙찰/유찰 리스트
-	public List<Bidinfo> getBidinfoList(int userno) {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		List<Bidinfo> list = new ArrayList<>();
-		try {
-			conn = DBUtil.getConnection();
-			String sql = "SELECT B.successbidno, A.userno, C.productno, C.productname, C.image, D.finalprice, B.isbid FROM user A\r\n" + 
-					"INNER JOIN successbidinfo B ON A.userno = B.userno\r\n" + 
-					"INNER JOIN product C ON B.productno = C.productno\r\n" + 
-					"INNER JOIN copy_auction D ON B.auctioncopyno = D.auctionno\r\n" + 
-					"WHERE A.userno = ?";
-			stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, userno);
-			rs = stmt.executeQuery();
-
-			while (rs.next()) {
-				Integer bidnoParse = rs.getObject("successbidno", Integer.class);
-				Integer usernoParse = rs.getObject("userno", Integer.class);
-				Integer productnoParse = rs.getObject("productno", Integer.class);
-				String productname = rs.getString("productname");
-				Blob imageBlob = rs.getBlob("image");
-				
-				byte[] imageBytes = imageBlob.getBytes(1, (int)imageBlob.length());
-				ImageIcon imageIcon = setImage(imageBytes);
-				
-				int finalprice = rs.getInt("finalprice");
-				
-				int bidint = rs.getInt("isbid");
-				boolean bidParse = (bidint == 1);
-				
-				list.add(new Bidinfo(bidnoParse, usernoParse, productnoParse, productname, imageIcon, finalprice, bidParse));
-			}
-			return list;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			DBUtil.close(rs);
-			DBUtil.close(stmt);
-			DBUtil.close(conn);
-		}
-		return null;
-	}
+//	public List<Bidinfo> getBidinfoList(int userno) {
+//		Connection conn = null;
+//		PreparedStatement stmt = null;
+//		ResultSet rs = null;
+//		List<Bidinfo> list = new ArrayList<>();
+//		try {
+//			conn = DBUtil.getConnection();
+//			String sql = "SELECT B.successbidno, A.userno, C.productno, C.productname, C.image, D.finalprice, B.isbid FROM user A\r\n" + 
+//					"INNER JOIN successbidinfo B ON A.userno = B.userno\r\n" + 
+//					"INNER JOIN product C ON B.productno = C.productno\r\n" + 
+//					"INNER JOIN copy_auction D ON B.auctioncopyno = D.auctionno\r\n" + 
+//					"WHERE A.userno = ?";
+//			stmt = conn.prepareStatement(sql);
+//			stmt.setInt(1, userno);
+//			rs = stmt.executeQuery();
+//
+//			while (rs.next()) {
+//				Integer bidnoParse = rs.getObject("successbidno", Integer.class);
+//				Integer usernoParse = rs.getObject("userno", Integer.class);
+//				Integer productnoParse = rs.getObject("productno", Integer.class);
+//				String productname = rs.getString("productname");
+//				Blob imageBlob = rs.getBlob("image");
+//				
+//				byte[] imageBytes = imageBlob.getBytes(1, (int)imageBlob.length());
+//				ImageIcon imageIcon = setImage(imageBytes);
+//				
+//				int finalprice = rs.getInt("finalprice");
+//				
+//				int bidint = rs.getInt("isbid");
+//				boolean bidParse = (bidint == 1);
+//				
+//				list.add(new Bidinfo(bidnoParse, usernoParse, productnoParse, productname, imageIcon, finalprice, bidParse));
+//			}
+//			return list;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			DBUtil.close(rs);
+//			DBUtil.close(stmt);
+//			DBUtil.close(conn);
+//		}
+//		return null;
+//	}
 }
