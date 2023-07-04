cd9e9801a06f3131c3ab790f75b1866a3aca353c
minah <simplemina@naver>
2023-07-04 16:07:22 +0900

    매뉴얼 수정

--- 0623Start/src/secondweek/ManualFrame.java
+++ 0623Start/src/secondweek/ManualFrame.java
@@ -6,28 +6,92 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
-public class ManualFrame extends JFrame {
+//public class ManualFrame extends JFrame {
+//    public ManualFrame() {
+//        setTitle("Image Panel Example");
+//        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        setSize(600, 500);
+//        setLocationRelativeTo(null);
+//
+//        JPanel panel = new JPanel();
+//        getContentPane().add(panel, BorderLayout.CENTER);
+//        panel.setLayout(null);
+//
+//     
+//        ImageIcon imageIcon = new ImageIcon("img/manual.gif");
+//
+//        // 이미지를 표시할 JLabel 생성
+//        JLabel imageLabel = new JLabel(imageIcon);
+//        imageLabel.setBounds(0, 0, imageIcon.getIconWidth(), imageIcon.getIconHeight());
+//
+//        // 패널에 이미지 레이블 추가
+//        panel.add(imageLabel);
+//
+//        setVisible(true);
+//    }
+//
+//}
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class ManualFrame {
+    private JFrame frame;
+    private JLabel imageLabel;
+    private ArrayList<String> imagePaths;
+    private int currentImageIndex;
+
     public ManualFrame() {
-        setTitle("Image Panel Example");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setSize(600, 500);
-        setLocationRelativeTo(null);
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel();
-        getContentPane().add(panel, BorderLayout.CENTER);
-        panel.setLayout(null);
+        imagePaths = new ArrayList<>();
+        imagePaths.add("img/manu1.jpg");
+        imagePaths.add("img/manu2.jpg");
+        imagePaths.add("img/manu3.jpg");
+        imagePaths.add("img/manu4.jpg");
+        imagePaths.add("img/manu5.jpg");
+        imagePaths.add("img/manu6.jpg");
+        imagePaths.add("img/manu7.jpg");
+        imagePaths.add("img/manu8.jpg");
+        imagePaths.add("img/manu9.jpg");
+        imagePaths.add("img/manu10.jpg");
+        imagePaths.add("img/manu11.jpg");
+        imagePaths.add("img/manu12.jpg");
+      
 
-     
-        ImageIcon imageIcon = new ImageIcon("img/manual.gif");
+        currentImageIndex = 0;
 
-        // 이미지를 표시할 JLabel 생성
-        JLabel imageLabel = new JLabel(imageIcon);
-        imageLabel.setBounds(0, 0, imageIcon.getIconWidth(), imageIcon.getIconHeight());
+        imageLabel = new JLabel();
+        updateImage();
 
-        // 패널에 이미지 레이블 추가
-        panel.add(imageLabel);
+        JButton nextButton = new JButton("Next");
+  
+    
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentImageIndex = (currentImageIndex + 1) % imagePaths.size();
+                updateImage();
+            }
+        });
 
-        setVisible(true);
+        frame.add(imageLabel);
+        frame.add(nextButton, BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
+
+    private void updateImage() {
+        String imagePath = imagePaths.get(currentImageIndex);
+        ImageIcon imageIcon = new ImageIcon(imagePath);
+        imageLabel.setIcon(imageIcon);
+        frame.revalidate();
+    }
+
 
 }
